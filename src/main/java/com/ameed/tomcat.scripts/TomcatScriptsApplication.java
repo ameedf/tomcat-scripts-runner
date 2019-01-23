090732b67bb3333f124e5ac9107c98aa0045c302
ameed_f <ameed_f@malam.com>
2019-01-23 14:28:12 +0200

    First commit

---
+++ src/main/java/com/ameed/tomcat.scripts/TomcatScriptsApplication.java
@@ -0,0 +1,14 @@
+package com.ameed.tomcatscripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatScriptsApplication.class, args);
+	}
+
+}
+
